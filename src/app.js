4865ab9c71231b101feb0e58e8dee3e80d44df52
cjrj <crivasjayo@gmail.com>
2022-02-06 16:54:35 -0400

    :four_leaf_clover: Entrega de proyecto

--- src/app.js
+++ src/app.js
@@ -5,7 +5,42 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const cardSuits = ["♦", "♥", "♠", "♣"];
+const cardNumbers = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+
+const suitColor = ["#FC0D1C", "#000000"];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const getRandomSuit = cardSuits[Math.floor(Math.random() * cardSuits.length)];
+  const getRandomCardNumber =
+    cardNumbers[Math.floor(Math.random() * cardNumbers.length)];
+  const getRandomColor =
+    suitColor[Math.floor(Math.random() * suitColor.length)];
+
+  const randomCard = document.querySelectorAll(".suit");
+
+  randomCard.forEach(
+    drawSuit => (drawSuit.innerHTML = `<p>${getRandomSuit}</p>`)
+  );
+  randomCard.forEach(drawColor => (drawColor.style.color = getRandomColor));
+
+  const cardValue = document.querySelectorAll(".card-value");
+
+  cardValue.forEach(
+    drawNumber => (drawNumber.innerHTML = `<h1>${getRandomCardNumber}</h1>`)
+  );
 };
